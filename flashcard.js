1ff8cb88d03f558a279fa1b788cccd8e53b5c059
djrloria <djrloria@gmail.com>
2017-05-16 13:35:58 -0500

    initial code. main code for basiccard and clozecard created.

---
+++ flashcard.js
@@ -0,0 +1,43 @@
+function BasicCard(question, answer) {
+    if (this instanceof BasicCard) {
+        this.front = question;
+        this.back = answer;
+    }
+    else {
+        return new BasicCard(question, answer);
+    }
+};
+
+function ClozeCard(question, answer) {
+    if (this instanceof ClozeCard) {
+        this.cloze = answer;
+        this.fullText = question;
+        this.partial = question.replace(answer, "...");
+    }
+    else {
+        return new ClozeCard(question, answer);
+    }
+}
+
+var firstPresident = BasicCard("Who was the first president of the United States?", "George Washington");
+
+// "Who was the first president of the United States?"
+console.log(firstPresident.front); 
+
+// "George Washington"
+console.log(firstPresident.back); 
+
+var firstPresidentCloze = new ClozeCard(
+    "George Washington was the first president of the United States.", "George Washington");
+
+// George Washington
+console.log(firstPresidentCloze.cloze); 
+
+// ... was the first president of the United States.
+console.log(firstPresidentCloze.partial); 
+
+// George Washington was the first president of the United States.
+console.log(firstPresidentCloze.fullText); 
+
+// Should throw or log an error because "oops" doesn't appear in "This doesn't work"
+//var brokenCloze("This doesn't work", "oops"); 
